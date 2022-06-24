dc8273bd1c04367a4676d6e24381ba31e8418e5e
Jack Danzey <jackdanzey@gmail.com>
2022-06-24 16:53:14 -0400

    adds views, index/show in books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    render template: "books/index"
+  end
+
+  def show
+    @book = Book.find_by(id: params["id"])
+    render template: "books/show"
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,5 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
 end
