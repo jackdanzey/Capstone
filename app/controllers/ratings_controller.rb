902050555d8e07c13684ecc01fbf838bd3d9f9fd
Jack Danzey <jackdanzey@gmail.com>
2022-07-12 12:17:44 -0400

    no idea

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,9 +1,12 @@
 class RatingsController < ApplicationController
+  # def create
+  #   rating = Rating.create!(
+  #     user_id: current_user.id,
+  #     book_id: params[:book_id],
+  #     score: params[:score],
+  #   )
+  # end
   def create
-    rating = Rating.create!(
-      user_id: current_user.id,
-      book_id: params[:book_id],
-      score: params[:score],
-    )
+    Rating.find_or_initialize_by(user_id: current_user.id).update!(score: params[:score], book_id: params[:book_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get "/posts" => "posts#index"
 
   post "/ratings" => "ratings#create"
+  patch "/ratings" => "ratings#update"
 end
